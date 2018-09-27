d63811d5ecf314997d40dc0387f88e9bba7f14f0
Rapidnack <rapidnack@gmail.com>
2018-09-28 06:56:32 +0900

    Uses SPI1

--- Rapidnack.Net/XPiGpiodIf.cs
+++ Rapidnack.Net/XPiGpiodIf.cs
@@ -752,7 +752,7 @@ Or to take Arms against a Sea of troubles,
 
 			/* this test requires a MCP3202 on SPI channel 1 */
 
-			h = pigpiodIf.spi_open(1, 50000, 0);
+			h = pigpiodIf.spi_open(2, 50000, 256 + 0);
 			CHECK(12, 1, h, 0, 0, "spi open", ct);
 
 
